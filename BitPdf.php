6e07ef53ef0e8a4e9b69c243ef0b33fe75410281
Lester Caine <lester@lsces.co.uk>
2005-08-07 16:25:59 +0000

    Make ->genDb()-> work with php4 change back to ->mDB->

--- BitPdf.php
+++ BitPdf.php
@@ -3,7 +3,7 @@
 * Pdf system class for outputing pdf file images
 *
 * @author   
-* @version  $Revision: 1.1.1.1.2.2 $
+* @version  $Revision: 1.1.1.1.2.3 $
 * @package  pdf
 */
 
@@ -83,7 +83,7 @@ class BitPdf extends Cezpdf
 		global $gBitSystem;
 		$ret = NULL;
 		$query = "SELECT `name`,`package`,`value` FROM `" . BIT_DB_PREFIX . "tiki_preferences` WHERE `name` LIKE ?";
-		$result = $gBitSystem->getDb()->query($query,array('feature_%_generate_pdf'));
+		$result = $gBitSystem->mDb->query($query,array('feature_%_generate_pdf'));
 		while( $res = $result->fetchRow() ) {
 			$ret[] = $res;
 		}
