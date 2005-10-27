b57ee5162d2582fecd522a8a5e204fd0b3be839b
Max Kremmel <xing@synapse.plus.com>
2005-10-27 14:20:41 +0000

    "cleaned up all schema_inc.php files - used <pkg>_PKG_NAME instead of '<pkg>' and moved registerSchemaDefaults() to registerPreferences() and registerUserPermission() where appropriate"

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,6 +1,4 @@
 <?php
-
-
 $gBitInstaller->registerPackageInfo( PDF_PKG_NAME, array(
 	'description' => "PDF generation package for creating PDF files from Liberty content.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
@@ -9,13 +7,8 @@ $gBitInstaller->registerPackageInfo( PDF_PKG_NAME, array(
 	'dependencies' => 'html',
 ) );
 
-
 // ### Default UserPermissions
 $gBitInstaller->registerUserPermissions( PDF_PKG_NAME, array(
 	array('bit_p_pdf_generation', 'Can create PDF files from content', 'registered', PDF_PKG_NAME),
 ) );
-
-
-
-
 ?>
