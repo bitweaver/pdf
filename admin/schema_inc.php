fc0c73d6d99fe1ca04e2e8f9672c16fa92157909
Max Kremmel <xing@synapse.plus.com>
2006-02-05 21:20:10 +0000

    remove unused parameters in registerPackageInfo

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -2,9 +2,6 @@
 $gBitInstaller->registerPackageInfo( PDF_PKG_NAME, array(
 	'description' => "PDF generation package for creating PDF files from Liberty content.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
-	'version' => '0.1',
-	'state' => 'beta',
-	'dependencies' => 'html',
 ) );
 
 // ### Default UserPermissions
