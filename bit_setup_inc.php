ebed61fe83aa5a534ef05b4647bce9d40362629f
Max Kremmel <xing@synapse.plus.com>
2006-02-17 23:51:44 +0000

    registerPackage now takes a hash instead of multiple parameters - keeps # of parameters under control

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,6 +1,13 @@
 <?php
 global $gBitSystem;
-$gBitSystem->registerPackage( 'pdf', dirname( __FILE__ ).'/', TRUE, LIBERTY_SERVICE_DOCUMENT_GENERATION );
+
+$registerHash = array(
+	'package_name' => 'pdf',
+	'package_path' => dirname( __FILE__ ).'/',
+	'service' => LIBERTY_SERVICE_DOCUMENT_GENERATION,
+);
+$gBitSystem->registerPackage( $registerHash );
+
 if( $gBitSystem->isPackageActive( 'pdf' ) ) {
 	$gLibertySystem->registerService( LIBERTY_SERVICE_DOCUMENT_GENERATION, PDF_PKG_NAME, array(
 		'content_icon_tpl' => 'bitpackage:pdf/pdf_service_icons.tpl',
